85b99790345285dcf4caa6bad3e47d52801b08b9
RuralAnemone ‮ <me.awesomerest@gmail.com>
2023-04-14 19:38:59 +0000

    add uptime api idk

---
+++ src/pages/api/uptime.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  time: number;
+};
+
+export default function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ time: process.uptime() });
+}
