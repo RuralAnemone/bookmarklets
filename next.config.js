1cee46d92e48b82d214a7bc875377f9c5bdf8ac7
RuralAnemone ‮ <me.awesomerest@gmail.com>
2023-04-14 01:59:12 +0000

    imgix cdn

--- next.config.js
+++ next.config.js
@@ -3,4 +3,30 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+// begin section: from https://www.viget.com/articles/host-build-and-deploy-next-js-projects-on-github-pages/
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+// end section
+
+module.exports = {
+  ...nextConfig,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'ruralanemone'
+
+  }
+}
