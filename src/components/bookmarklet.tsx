9b151c7e2892ca49faabcd03446c03e7f8b4eb7f
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2023-06-04 13:39:07 -0500

    (:

--- src/components/bookmarklet.tsx
+++ src/components/bookmarklet.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Bookmarklet(props: any) {
 	const code: string = `javascript:fetch("https://cdn.jsdelivr.net/gh/RuralAnemone/bookmarklets/public/${props.name}.js").then((data=>data.text())).catch((e=>alert(e))).then((text=>eval(text))).catch((e=>alert(e)));`
 	return (
-		<div className="border-2 border-x-white p-10 m-10 text-center">
+		<div className="border-2 border-x-white rounded p-10 m-10 text-center bg-zinc-900">
 		<a className="text-2xl underline text-blue-400" href={code}><h3>{props.name}</h3></a>
 			<p>{props.desc}</p>
 		</div>
