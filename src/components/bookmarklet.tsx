7016867c65433ca213b9d48eb13c9671c7d023ff
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2023-06-04 12:55:04 -0500

    border moment (10x better design (real))

--- src/components/bookmarklet.tsx
+++ src/components/bookmarklet.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Bookmarklet(props: any) {
 	const code: string = `javascript:fetch("https://cdn.jsdelivr.net/gh/RuralAnemone/bookmarklets/public/${props.name}.js").then((data=>data.text())).catch((e=>alert(e))).then((text=>eval(text))).catch((e=>alert(e)));`
 	return(
-		<div>
-			<a className="text-2xl underline text-blue-400" href={code}><h3>{props.name}</h3></a>
+		<div className="border-2 border-x-white p-10 m-10">
+		<a className="text-2xl underline text-blue-400" href={code}><h3>{props.name}</h3></a>
 			<p>{props.desc}</p>
 		</div>
 	);
