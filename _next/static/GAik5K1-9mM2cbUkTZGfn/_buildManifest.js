c41ea8530cc6ae7587492a80aa66d7baea2749b9
RuralAnemone <64103005+RuralAnemone@users.noreply.github.com>
2023-06-04 17:57:01 +0000

    Deploying to gh-pages from @ RuralAnemone/bookmarklets@7016867c65433ca213b9d48eb13c9671c7d023ff 🚀

--- _next/static/GAik5K1-9mM2cbUkTZGfn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f131a50717fcb942.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
