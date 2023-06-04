3ee241dfdd7115750f8b91606f4891558d1bbdcb
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2023-06-04 14:01:23 -0500

    lnik :3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main>
       <Bookmarklet name="darkmode" desc={`uses DarkReader's api to change a site to darkmode (useful if you can't install extensions)`} />
-      <Bookmarklet name="freenom" desc={`perform fast-checkout on freenom or something. see rammerhead docs for more info`} />
+      <Bookmarklet name="freenom" desc={`perform fast-checkout on freenom or something. see rammerhead docs (bit.ly/rammerhead-docs) for more info`} />
       <Bookmarklet name="kahoot" desc={`(:`} />
       <Bookmarklet name="screenshot" desc={`takes a screenshot of the current page. why? idk someone in an issue needed to take a screenshot for a bug report or something but their disctrict blocked screenshots of all things, so I made this for them and they never used it but I still do for fun...`} />
       <Bookmarklet name="ytdl" desc={`highly configurable bookmarklet for downloading videos (real)`} />
